bd4183bde8926d68b53a465c2e0dca217e86ddee
id4thomas <id2thomas@gmail.com>
2018-05-31 19:11:31 +0900

    Class for fetching seat info

---
+++ app/src/main/java/edu/skku/swp3/metroapp/FetchSeat.java
@@ -0,0 +1,31 @@
+package edu.skku.swp3.metroapp;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+/**
+ * Created by TS on 2018. 5. 31..
+ */
+
+public class FetchSeat extends Thread {
+
+    public FetchSeat(){
+
+    }
+    public void run()
+    {
+
+        try {
+            Socket soc = new Socket("localhost", 5000);
+            ObjectInputStream ois = new ObjectInputStream (soc.getInputStream());
+           // ois.readObject(); //read here
+            ois.close();
+            soc.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
