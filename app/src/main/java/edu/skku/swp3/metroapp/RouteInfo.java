e95b3efc1ca182a3529cfbd0dd17b7839f70a9e7
lufovic77 <lufovic77@naver.com>
2018-06-05 23:12:02 +0900

    route info

--- app/src/main/java/edu/skku/swp3/metroapp/RouteInfo.java
+++ app/src/main/java/edu/skku/swp3/metroapp/RouteInfo.java
@@ -2,12 +2,14 @@ package edu.skku.swp3.metroapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.StringTokenizer;
 
 /**
@@ -21,6 +23,10 @@ public class RouteInfo extends Activity {
     private TextView arTime;
     private TextView deName;
     private TextView arName;
+    private long minutes;
+    private long si;
+    private long bun;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +47,27 @@ public class RouteInfo extends Activity {
         }
         deName.setText(station.get(0));
         arName.setText(station.get(1));
+        deName.setTextColor(Color.BLACK);
+        arName.setTextColor(Color.BLACK);
+
+        Calendar calendar = Calendar.getInstance();
+        this.minutes = calendar.get(Calendar.MINUTE)+calendar.get(Calendar.HOUR_OF_DAY)*60;
+        for(int i=0;i<10;i++){
+            this.minutes++;
+            if(this.minutes%5==0)
+                break;
+        }
+        this.si=this.minutes/60;
+        this.bun=this.minutes%60;
+        String text="";
+        text+=si+":"+bun;
+        deTime.setText(text);
+
+        long now = System.currentTimeMillis();
+
+        Toast.makeText(this, "!!"+this.minutes, Toast.LENGTH_LONG).show();
+
+
 
     }
 }
