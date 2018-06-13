6b91f13b0e999ee649dbcdd66e8bb76e2dd3aaf7
Jinsun <yds9744@naver.com>
2018-06-13 13:11:24 +0900

    Update MetroClass.java
    fix some, and fill update function

--- app/src/main/java/edu/skku/swp3/metroapp/MetroClass.java
+++ app/src/main/java/edu/skku/swp3/metroapp/MetroClass.java
@@ -27,20 +27,11 @@ import java.net.Socket;
 public class MetroClass extends Activity{
     private Spinner carSpin;
     private Button update;
+    private TextView metroinfo;
     private Integer[] SeatId = { R.id.left_seat1, R.id.left_seat2, R.id.left_seat3, R.id.left_seat4, R.id.left_seat5
-            , R.id.right_seat1, R.id.right_seat2, R.id.right_seat3, R.id.right_seat4, R.id.right_seat5};
+                                , R.id.right_seat1, R.id.right_seat2, R.id.right_seat3, R.id.right_seat4, R.id.right_seat5};
     private Button SeatBtn[] = new Button[10];
 
-    //car[0] = 비고
-    //car[1]~[10] = 해당 호차의 좌석정보
-    //car[][0]~[][4] = left seat의 좌석정보
-    //car[][5]~[][9] = right seat의 좌석정보
-    //seat state는 0: no one, 1: some one, 2: me!
-    //off_time 내리는 시간(if seat state = 1)
-    //off_station 내리는 역(if seat state = 1)
-    private Integer[][] car = new Integer[11][10];
-    private Integer[][] off_time = new Integer[11][10];
-    private String[][] off_station = new String[11][10];
     private boolean seat_myself = false; //whether I seat or not.(I can seat only one.)
     private int present_car = 1;
     private Integer[] whereIseat = new Integer[2];
@@ -53,20 +44,15 @@ public class MetroClass extends Activity{
         Intent intent = getIntent();
         seats= (SeatClass) intent.getSerializableExtra("seatInstance");
         setContentView(R.layout.metro);
-        car= new Integer[11][10];
-        //get intent(with 행성지 이름, 호차)=>textview설정해주기
 
         carSpin = (Spinner)findViewById(R.id.carSpinner);
         update = (Button)findViewById(R.id.update);
+        metroinfo = (TextView)findViewById(R.id.metroinfoText);
         for(int i=0;i<10;i++){
             SeatBtn[i]=(Button)findViewById(SeatId[i]);
 
         }
-        for(int j=1;j<11;j++){
-            for(int i=0;i<10;i++) {
-                car[j][i] = 0;
-            }
-        }
+
         carSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
@@ -77,7 +63,7 @@ public class MetroClass extends Activity{
                 str.substring(0, str.length()-2);
                 present_car = Integer.parseInt(str.substring(0,1));
                 Log.i("value:",Integer.toString(present_car));
-                car[present_car]=seats.getseat(present_car);
+                seats.car[present_car] = seats.getseat(present_car);
                 seatUpdate(); //car[present]에 해당하는 정보에 맞게 좌석이미지 update하는 함수 호출
 
                 Log.e("Selected item : ",str);
@@ -92,89 +78,93 @@ public class MetroClass extends Activity{
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                seats.car[present_car] = seats.getseat(present_car);
+                if(seat_myself) seats.car[whereIseat[0]][whereIseat[1]] = 2;
                 seatUpdate();
             }
         });
 
+        metroinfo.setText(seats.lane + seats.updown);
+
         SeatBtn[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][0] == 0) noone_popUp(v, 0);
-                else if(car[present_car][0] == 1) someone_popUp(v, 0);
-                else if(car[present_car][0] == 2) my_popUp(v, 0);
+                if(seats.car[present_car][0] == 0) noone_popUp(v, 0);
+                else if(seats.car[present_car][0] == 1) someone_popUp(v, 0);
+                else if(seats.car[present_car][0] == 2) my_popUp(v, 0);
             }
         });
         SeatBtn[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][1] == 0) noone_popUp(v, 1);
-                else if(car[present_car][0] == 1) someone_popUp(v, 1);
-                else if(car[present_car][1] == 2) my_popUp(v, 1);
+                if(seats.car[present_car][1] == 0) noone_popUp(v, 1);
+                else if(seats.car[present_car][1] == 1) someone_popUp(v, 1);
+                else if(seats.car[present_car][1] == 2) my_popUp(v, 1);
             }
         });
         SeatBtn[2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][2] == 0) noone_popUp(v, 2);
-                else if(car[present_car][0] == 1) someone_popUp(v, 2);
-                else if(car[present_car][2] == 2) my_popUp(v, 2);
+                if(seats.car[present_car][2] == 0) noone_popUp(v, 2);
+                else if(seats.car[present_car][2] == 1) someone_popUp(v, 2);
+                else if(seats.car[present_car][2] == 2) my_popUp(v, 2);
             }
         });
         SeatBtn[3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][3] == 0) noone_popUp(v, 3);
-                else if(car[present_car][0] == 1) someone_popUp(v, 3);
-                else if(car[present_car][3] == 2) my_popUp(v, 3);
+                if(seats.car[present_car][3] == 0) noone_popUp(v, 3);
+                else if(seats.car[present_car][3] == 1) someone_popUp(v, 3);
+                else if(seats.car[present_car][3] == 2) my_popUp(v, 3);
             }
         });
         SeatBtn[4].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][4] == 0) noone_popUp(v, 4);
-                else if(car[present_car][0] == 1) someone_popUp(v, 4);
-                else if(car[present_car][4] == 2) my_popUp(v, 4);
+                if(seats.car[present_car][4] == 0) noone_popUp(v, 4);
+                else if(seats.car[present_car][4] == 1) someone_popUp(v, 4);
+                else if(seats.car[present_car][4] == 2) my_popUp(v, 4);
             }
         });
 
         SeatBtn[5].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][5] == 0) noone_popUp(v, 5);
-                else if(car[present_car][0] == 1) someone_popUp(v, 5);
-                else if(car[present_car][5] == 2) my_popUp(v, 5);
+                if(seats.car[present_car][5] == 0) noone_popUp(v, 5);
+                else if(seats.car[present_car][5] == 1) someone_popUp(v, 5);
+                else if(seats.car[present_car][5] == 2) my_popUp(v, 5);
             }
         });
         SeatBtn[6].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][6] == 0) noone_popUp(v, 6);
-                else if(car[present_car][0] == 1) someone_popUp(v, 6);
-                else if(car[present_car][6] == 2) my_popUp(v, 6);
+                if(seats.car[present_car][6] == 0) noone_popUp(v, 6);
+                else if(seats.car[present_car][6] == 1) someone_popUp(v, 6);
+                else if(seats.car[present_car][6] == 2) my_popUp(v, 6);
             }
         });
         SeatBtn[7].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][7] == 0) noone_popUp(v, 7);
-                else if(car[present_car][0] == 1) someone_popUp(v, 7);
-                else if(car[present_car][7] == 2) my_popUp(v, 7);
+                if(seats.car[present_car][7] == 0) noone_popUp(v, 7);
+                else if(seats.car[present_car][7] == 1) someone_popUp(v, 7);
+                else if(seats.car[present_car][7] == 2) my_popUp(v, 7);
             }
         });
         SeatBtn[8].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][8] == 0) noone_popUp(v, 8);
-                else if(car[present_car][0] == 1) someone_popUp(v, 8);
-                else if(car[present_car][8] == 2) my_popUp(v, 8);
+                if(seats.car[present_car][8] == 0) noone_popUp(v, 8);
+                else if(seats.car[present_car][8] == 1) someone_popUp(v, 8);
+                else if(seats.car[present_car][8] == 2) my_popUp(v, 8);
             }
         });
         SeatBtn[9].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(car[present_car][9] == 0) noone_popUp(v, 9);
-                else if(car[present_car][0] == 1) someone_popUp(v, 9);
-                else if(car[present_car][9] == 2) my_popUp(v, 9);
+                if(seats.car[present_car][9] == 0) noone_popUp(v, 9);
+                else if(seats.car[present_car][9] == 1) someone_popUp(v, 9);
+                else if(seats.car[present_car][9] == 2) my_popUp(v, 9);
             }
         });
     }
@@ -182,7 +172,7 @@ public class MetroClass extends Activity{
     public void seatUpdate(){
         //left seat image update
         for(int i=0; i<5; i++){
-            switch(car[present_car][i]){
+            switch(seats.car[present_car][i]){
                 case 0:
                     SeatBtn[index].setBackgroundResource(R.drawable.lseat_noone);
                     break;
@@ -196,7 +186,7 @@ public class MetroClass extends Activity{
         }
         //right seat image update
         for(int i=5; i<10; i++){
-            switch(car[present_car][i]){
+            switch(seats.car[present_car][i]){
                 case 0:
                     SeatBtn[index].setBackgroundResource(R.drawable.rseat_noone);
                     break;
@@ -217,12 +207,16 @@ public class MetroClass extends Activity{
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 //앉았다고 표시한 자리가 있었으면, 그 자리 0(no_one)으로 set.
-                if(seat_myself) car[whereIseat[0]][whereIseat[1]] = 0;
-                //앉은 자리 whereIseat에 표시
+                if(seat_myself){
+                    seats.car[whereIseat[0]][whereIseat[1]] = 0;
+                    seats.updateseat(whereIseat[0], whereIseat[1], 0);
+                }
+                //앉은 자리 whereIseat에 표시&update
                 whereIseat[0] = present_car;
                 whereIseat[1] = index;
+                seats.updateseat(whereIseat[0], whereIseat[1], 1); //다른 사용자에게는 someone_seat으로 보여야함
                 //선택한 자리 2(myself)로 바꾸는 작업
-                car[present_car][index] = 2;
+                seats.car[present_car][index] = 2;
                 seat_myself = true;
                 if(index<5){
                     SeatBtn[index].setBackgroundResource(R.drawable.lseat_myself);
@@ -239,8 +233,7 @@ public class MetroClass extends Activity{
         index = arg;
         android.widget.PopupWindow popup = new android.widget.PopupWindow(MetroClass.this);
         android.widget.TextView text = (TextView)findViewById(R.id.SomeonePopup);
-        text.setText(off_time[present_car][index].toString() + off_station[present_car][index]);
-
+        //text.setText(seats.off_time[present_car][index].toString() + seats.off_station[present_car][index]);
         View layout = getLayoutInflater().inflate(R.layout.popup_content, null);
         popup.setContentView(layout);
         // Set content width and height
@@ -253,6 +246,7 @@ public class MetroClass extends Activity{
         //popup.setBackgroundDrawable(new android.graphics.drawable.BitmapDrawable());
         popup.showAsDropDown(v);
     }
+
     public void my_popUp(View v, int arg){
         index = arg;
         PopupMenu popup = new PopupMenu(getApplicationContext(), v);
@@ -261,7 +255,7 @@ public class MetroClass extends Activity{
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 //선택한 자리 0(no_one)으로 바꾸는 작업
-                car[present_car][index] = 0;
+                seats.car[present_car][index] = 0;
                 seat_myself = false;    //앉은 자리 없다고 다시 표시
                 if(index<5){
                     SeatBtn[index].setBackgroundResource(R.drawable.lseat_noone);
